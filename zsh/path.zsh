35565de024c48d2305298c8c1f9220e243cb8be2
zillafeng <zillafeng@tencent.com>
2020-05-08 20:09:25 +0800

    linux

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,7 +1,8 @@
-export PATH=$PATH:/home/z/Projects/open/depot_tools
-export PATH=$PATH:/home/z/Tools/swift-4.2.3-RELEASE-ubuntu18.04/usr/bin
+export PATH=$PATH:/home/z/Tools/depot_tools
 export PATH=$PATH:/home/z/.bin
-export PATH=$PATH:/home/z/Tools/ida
+export PATH=$PATH:/home/z/Projects/codeql-home/codeql-cli
+
+export FD=/home/z/Projects/fuzz
 
 # java
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
@@ -12,7 +13,3 @@ export CLASSPATH=$JAVA_HOME/lib/jna-5.3.1.jar:.
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Projects/py
 source /home/z/.local/bin/virtualenvwrapper.sh
-
-export PATH="/home/z/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
