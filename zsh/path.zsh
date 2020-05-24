44f0fd5952abcf6fcb8a00bd83e929ab172ddea0
zillafeng <zillafeng@tencent.com>
2020-05-24 12:32:50 +0800

    add pop() and binja

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,6 +1,7 @@
 export PATH=$PATH:/Users/z/.bin
 export PATH=$PATH:/Users/z/.local/bin
 export PATH=$PATH:/Applications/ida/ida.app/Contents/MacOS
+export PATH=$PATH:"/Applications/Binary Ninja.app/Contents/MacOS"
 export PATH=$PATH:/Users/z/Tools/ghidra_9.0.4/support
 export PATH=$PATH:/Users/z/Tools/depot_tools
 export PATH=$PATH:/Users/z/Projects/codeql-home/codeql-cli
